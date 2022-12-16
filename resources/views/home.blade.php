feabca08a32b4540db5ec0cb6be3da2438d99a00
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-12-16 18:24:42 +0100

    fix img card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,40 +2,39 @@
 
 @section('content')
 
-        <div class="row">
-
-            @foreach($movies as $key => $movie)
-            <div class="col-md-4">
-                <div class="profile-card-6"><img src="{{$movie['image']}}">
-                    <div class="profile-name">
-                        {{$movie['original_title']}}
-                    </div>
-                    <div class="profile-position">
-
-                    </div>
-                    <div class="profile-overview">
+        <div class="container">
+            <div class="row">
+                @foreach($movies as $key => $movie)
+                <div class="col-md-4">
+                    <div class="profile-card-6">
+                        <img src="{{$movie['image']}}">
+                        <div class="profile-name">
+                            {{$movie['original_title']}}
+                        </div>
+                        <div class="profile-position">
+                        </div>
                         <div class="profile-overview">
-                            <div class="row text-center">
-                                {{-- <div class="col-xs-4">
-                                    <h3>1</h3>
-                                    <p>Rank</p>
+                            <div class="profile-overview">
+                                <div class="row text-center">
+                                    {{-- <div class="col-xs-4">
+                                        <h3>1</h3>
+                                        <p>Rank</p>
+                                    </div>
+                                    <div class="col-xs-4">
+                                        <h3>50</h3>
+                                        <p>Matches</p>
+                                    </div>
+                                    <div class="col-xs-4">
+                                        <h3>35</h3>
+                                        <p>Goals</p>
+                                    </div> --}}
                                 </div>
-                                <div class="col-xs-4">
-                                    <h3>50</h3>
-                                    <p>Matches</p>
-                                </div>
-                                <div class="col-xs-4">
-                                    <h3>35</h3>
-                                    <p>Goals</p>
-                                </div> --}}
                             </div>
                         </div>
                     </div>
                 </div>
+                @endforeach
             </div>
-            @endforeach
-
-
         </div>
 
 @endsection
