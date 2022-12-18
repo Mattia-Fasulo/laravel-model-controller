0385308e8e2417351a9fb30502509456690d8da7
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-12-18 17:53:18 +0100

    added vote on card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-
-        <div class="container">
-            <div class="row">
-                @foreach($movies as $key => $movie)
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $key => $movie)
                 <div class="col-md-4">
                     <div class="profile-card-6">
-                        <img src="{{$movie['image']}}">
+                        <img src="{{ $movie['image'] }}">
                         <div class="profile-name">
-                            {{$movie['original_title']}}
+                            {{ $movie['original_title'] }}
                         </div>
                         <div class="profile-position">
                         </div>
                         <div class="profile-overview">
                             <div class="profile-overview">
                                 <div class="row text-center">
-                                    {{-- <div class="col-xs-4">
-                                        <h3>1</h3>
-                                        <p>Rank</p>
+                                    <div class="col-xs-4">
+                                        @for ($i = 0; $i < 5; $i++)
+                                            @if ($i < ceil($movie['vote']) / 2)
+                                                <img src="{{ Vite::asset('resources/img/star-solid.svg') }}"
+                                                    alt="star">
+                                            @else
+                                                <img src="{{ Vite::asset('resources/img/star-regular.svg') }}"
+                                                    alt="star">
+                                            @endif
+                                        @endfor
+
+
                                     </div>
+                                    {{--
                                     <div class="col-xs-4">
                                         <h3>50</h3>
                                         <p>Matches</p>
@@ -33,8 +42,7 @@
                         </div>
                     </div>
                 </div>
-                @endforeach
-            </div>
+            @endforeach
         </div>
-
+    </div>
 @endsection
